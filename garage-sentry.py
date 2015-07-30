2842f18bbb39d039be5aa1cd634d8c2fab947a48
Dan Ferrante <dan.ferrante@gmail.com>
2015-07-29 20:18:44 -0400

    some cleanup

--- garage-sentry.py
+++ garage-sentry.py
@@ -3,11 +3,17 @@ import RPIO
 import time
 import logging as log
 
-
+RPIO.setmode(RPIO.BOARD)
 log.basicConfig(filename='/var/log/garage.log', level=log.DEBUG, format="%(asctime)s %(levelname)s: %(message)s")
 
+
+SENSORS = {
+    15: 'Dan',
+    11: 'Victoria'
+}
+
 def sensor_event(chan, msg):
-    name = input_channels[chan]
+    name = SENSORS[chan]
     position = 'open' if RPIO.input(chan) else 'closed'
     if chan not in last_status or last_status['chan'] != position:
         last_status[chan] = position
@@ -15,14 +21,10 @@ def sensor_event(chan, msg):
 
 
 if __name__ == '__main__':
-    input_channels = {
-        22: 'Dan',
-        17: 'Victoria'
-    }
     last_status = {}
 
     log.info('script started, setting up handlers')
-    for chan, name in input_channels.iteritems():
+    for chan, name in SENSORS.iteritems():
         RPIO.add_interrupt_callback(chan, sensor_event, debounce_timeout_ms=100)
         log.info("set up handler for %s's door", name)
         sensor_event(chan)
