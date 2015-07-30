002d0a329789748901024b821673f962749a548c
Dan Ferrante <dan.ferrante@gmail.com>
2015-07-29 20:09:05 -0400

    changed to use RPIO

--- garage-sentry.py
+++ garage-sentry.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+import RPIO
 import time
 import logging as log
 
 
 log.basicConfig(filename='/var/log/garage.log', level=log.DEBUG, format="%(asctime)s %(levelname)s: %(message)s")
-GPIO.setmode(GPIO.BOARD)
 
-def sensor_event(chan):
+def sensor_event(chan, msg):
     name = input_channels[chan]
-    position = 'open' if GPIO.input(chan) else 'closed'
+    position = 'open' if RPIO.input(chan) else 'closed'
     if chan not in last_status or last_status['chan'] != position:
         last_status[chan] = position
-        log.info("%s's door is %s", name, position)
+        log.info("%s's door is %s: %s", name, position, msg)
 
 
 if __name__ == '__main__':
     input_channels = {
-        15: 'Dan',
-        11: 'Victoria'
+        22: 'Dan',
+        17: 'Victoria'
     }
     last_status = {}
 
     log.info('script started, setting up handlers')
     for chan, name in input_channels.iteritems():
-        GPIO.setup(chan, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(chan, GPIO.BOTH, callback=sensor_event)
+        RPIO.add_interrupt_callback(chan, sensor_event, debounce_timeout_ms=100)
         log.info("set up handler for %s's door", name)
         sensor_event(chan)
     log.info('setup complete')
 
-    while 1:
-        time.sleep(2)
+    RPIO.wait_for_interrupts()
